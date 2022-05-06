f1c2cdebcb2c58a817fbef0ec644c546fb46bd48
choiseungji <103562328+choiseungji@users.noreply.github.com>
2022-05-06 16:12:08 +0900

    Delete Strings and lists.py

--- code/Strings and lists.py
+++
@@ -1,9 +0,0 @@
-#Given: A string s of length at most 200 letters and four integers a, b, c and d.
-#Return: The slice of this string from indices a through b and c through d (with space in between), inclusively. In other words, we should include elements s[b] and s[d] in our slice.
-
-#the input
-# ==========================
-a = 'q2Hn9aObdfMpyhKWM2faWY7vLbz3P8pm5YdTESMOYC2mM4Dl0AaPyHFJYpYrbfsuTV7JPQZrXizpvMochlusPrDPUTDmJjpRUWS5EjMyTV5U7KzfMBMRngDFpfAqC09kz1NW2kKP9v29D4XClC0YxRKleDZ1KQKisabellinazo6YhBMROhynZ6jfpaa9aAo'
-print(a[77:83],a[159:168])
-
-#the output : Mochlu isabellin
