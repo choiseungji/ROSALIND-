8332c0a77fe8b406f4d1fd033ba2e9453d9d751f
choiseungji <103562328+choiseungji@users.noreply.github.com>
2022-05-06 16:08:29 +0900

    20220506

---
+++ code/Strings and Lists.py
@@ -0,0 +1,14 @@
+#Given: Two positive integers a and b (a<b<10000).
+#Return: The sum of all odd integers from a through b, inclusively.
+
+#input 
+
+s ='86zcstHpPhasianusKcujG4vHMLdFuHJa3BgvkC4QI2IAKTQFLdnbzOUjVkEHgQNRknCWpAeIKmnYfbVcoroneMeVa2JSl6DekBsIHBJTtJAvDQJUMcK2xaBCiqdT0Uxbifh4woGRSbtMqaSOJUJyTgM9qLbT48zBOyIt6wNG55GKfEAPpP0ouFbDDF2cbylsJWwk'
+a = 8
+b = 16
+c = 80
+d = 85
+e = s[a:b] +s[b] + " " + s[c:d] + s[d]
+print(e)
+
+#output > Phasianus corone
